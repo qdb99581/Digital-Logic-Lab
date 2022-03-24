513ad95b6ee0762af3cc43b2a2273b17c941758e
BrianLiang <t105310007@ntut.org.tw>
2022-03-24 18:35:36 +0800

    Update Braille.vhdl

--- Week 5/Braille.vhdl
+++ Week 5/Braille.vhdl
@@ -17,5 +17,5 @@ architecture Equations of Braille is
         V <= (A and C) or (A and B);
         W <= (not A and not B and C) or (not A and C and not D) or (A and not C) or (A and B and D) or (B and not C and D);
         X <= (not A and C and D) or (not A and B and not D) or (A and not C and D) or (B and C and not D);
-        Y <= 'Z';
+        Y <= 'Z'; -- '0' works as well
     end Equations;

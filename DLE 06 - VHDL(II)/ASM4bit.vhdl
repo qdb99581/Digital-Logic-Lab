2b15cc188d2d6281c428b2356b05a71c76b4503b
Tzu-Hung Liang <t110318094@ntut.org.tw>
2023-04-10 13:46:54 +0800

    Formatting VHDL

--- DLE 06 - VHDL(II)/ASM4bit.vhdl
+++ DLE 06 - VHDL(II)/ASM4bit.vhdl
@@ -1,27 +1,27 @@
 -- ASM4 stands for Adder-Subtracter-Multiplyer 4 bits
 
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity ASM4 is
-    port(
-        A, B: in std_logic_vector(3 downto 0);
-        F: in std_logic_vector(1 downto 0); -- Function selecting
-        S: out std_logic_vector(7 downto 0)
+ENTITY ASM4 IS
+    PORT (
+        A, B : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        F : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- Function selecting
+        S : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
     );
-end ASM4;
+END ASM4;
 
-architecture Equations of ASM4 is
-    signal A_int, B_int, results: integer := 0;
-    begin
-        A_int <= to_integer(unsigned(A));
-        B_int <= to_integer(unsigned(A));
-        with F select
-        results <= 
-            A_int + B_int when "00",
-            A_int - B_int when "01",
-            A_int * B_int when "10",
-            0 when others;
-        S <= std_logic_vector(to_unsigned(results, 8));
-end Equations;
+ARCHITECTURE Equations OF ASM4 IS
+    SIGNAL A_int, B_int, results : INTEGER := 0;
+BEGIN
+    A_int <= to_integer(unsigned(A));
+    B_int <= to_integer(unsigned(B));
+    WITH F SELECT
+        results <=
+        A_int + B_int WHEN "00",
+        A_int - B_int WHEN "01",
+        A_int * B_int WHEN "10",
+        0 WHEN OTHERS;
+    S <= STD_LOGIC_VECTOR(to_unsigned(results, 8));
+END Equations;
--- DLE 06 - VHDL(II)/comparator_4bit.vhdl
+++ DLE 06 - VHDL(II)/comparator_4bit.vhdl
@@ -1,80 +1,82 @@
 -- 4-bit Comparator
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity Comparator4 is
-    port (
-        X, Y: in std_logic_vector(3 downto 0);
-        C0: in std_logic_vector(1 downto 0);
-        C4: out std_logic_vector(1 downto 0));
-end Comparator4;
+ENTITY Comparator4 IS
+    PORT (
+        X, Y : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        C0 : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        C4 : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+END Comparator4;
 
-architecture Structure of Comparator4 is
-    component Comparator1
-        port (X, Y: in std_logic; 
-              Ci: in std_logic_vector(1 downto 0);
-              Ci_next: out std_logic_vector(1 downto 0));
-    end component;
+ARCHITECTURE Structure OF Comparator4 IS
+    COMPONENT Comparator1
+        PORT (
+            X, Y : IN STD_LOGIC;
+            Ci : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+            Ci_next : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+    END COMPONENT;
 
-    signal C1: std_logic_vector(1 downto 0);
-    signal C2: std_logic_vector(1 downto 0);
-    signal C3: std_logic_vector(1 downto 0);
+    SIGNAL C1 : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL C2 : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL C3 : STD_LOGIC_VECTOR(1 DOWNTO 0);
 
-    begin
-        FC0: Comparator1 port map(X(0), Y(0), C0, C1);
-        FC1: Comparator1 port map(X(1), Y(1), C1, C2);
-        FC2: Comparator1 port map(X(2), Y(2), C2, C3);
-        FC3: Comparator1 port map(X(3), Y(3), C3, C4);
-end Structure;
+BEGIN
+    FC0 : Comparator1 PORT MAP(X(0), Y(0), C0, C1);
+    FC1 : Comparator1 PORT MAP(X(1), Y(1), C1, C2);
+    FC2 : Comparator1 PORT MAP(X(2), Y(2), C2, C3);
+    FC3 : Comparator1 PORT MAP(X(3), Y(3), C3, C4);
+END Structure;
 
 -- 1-bit Comparator
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity Comparator1 is
-    port (X, Y: in std_logic;
-          Ci: in std_logic_vector(1 downto 0);
-          Ci_next: out std_logic_vector(1 downto 0));
-end Comparator1;
+ENTITY Comparator1 IS
+    PORT (
+        X, Y : IN STD_LOGIC;
+        Ci : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        Ci_next : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+END Comparator1;
 
-architecture Equations of Comparator1 is
-    signal input: std_logic_vector(3 downto 0);
-    begin
-        input <= Ci(0) & Ci(1) & X & Y;
+ARCHITECTURE Equations OF Comparator1 IS
+    SIGNAL input : STD_LOGIC_VECTOR(3 DOWNTO 0);
+BEGIN
+    input <= Ci(0) & Ci(1) & X & Y;
 
-        Ci_next(0) <= '0' when input = "0000" else
-            '0' when input = "0001" else
-            '1' when input = "0010" else
-            '0' when input = "0011" else
-            '0' when input = "0100" else
-            '0' when input = "0101" else
-            '1' when input = "0110" else
-            '0' when input = "0111" else
-            '1' when input = "1000" else
-            '0' when input = "1001" else
-            '1' when input = "1010" else
-            '1' when input = "1011" else
-            'X' when input = "1100" else
-            'X' when input = "1101" else
-            'X' when input = "1110" else
-            'X' when input = "1111";
+    Ci_next(0) <= '0' WHEN input = "0000" ELSE
+    '0' WHEN input = "0001" ELSE
+    '1' WHEN input = "0010" ELSE
+    '0' WHEN input = "0011" ELSE
+    '0' WHEN input = "0100" ELSE
+    '0' WHEN input = "0101" ELSE
+    '1' WHEN input = "0110" ELSE
+    '0' WHEN input = "0111" ELSE
+    '1' WHEN input = "1000" ELSE
+    '0' WHEN input = "1001" ELSE
+    '1' WHEN input = "1010" ELSE
+    '1' WHEN input = "1011" ELSE
+    'X' WHEN input = "1100" ELSE
+    'X' WHEN input = "1101" ELSE
+    'X' WHEN input = "1110" ELSE
+    'X' WHEN input = "1111";
 
-        Ci_next(1) <= '0' when input = "0000" else
-            '1' when input = "0001" else
-            '0' when input = "0010" else
-            '0' when input = "0011" else
-            '1' when input = "0100" else
-            '1' when input = "0101" else
-            '0' when input = "0110" else
-            '1' when input = "0111" else
-            '0' when input = "1000" else
-            '1' when input = "1001" else
-            '0' when input = "1010" else
-            '0' when input = "1011" else
-            'X' when input = "1100" else
-            'X' when input = "1101" else
-            'X' when input = "1110" else
-            'X' when input = "1111";
-end Equations;
+    Ci_next(1) <= '0' WHEN input = "0000" ELSE
+    '1' WHEN input = "0001" ELSE
+    '0' WHEN input = "0010" ELSE
+    '0' WHEN input = "0011" ELSE
+    '1' WHEN input = "0100" ELSE
+    '1' WHEN input = "0101" ELSE
+    '0' WHEN input = "0110" ELSE
+    '1' WHEN input = "0111" ELSE
+    '0' WHEN input = "1000" ELSE
+    '1' WHEN input = "1001" ELSE
+    '0' WHEN input = "1010" ELSE
+    '0' WHEN input = "1011" ELSE
+    'X' WHEN input = "1100" ELSE
+    'X' WHEN input = "1101" ELSE
+    'X' WHEN input = "1110" ELSE
+    'X' WHEN input = "1111";
+END Equations;

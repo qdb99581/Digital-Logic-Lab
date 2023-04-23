ad513228c5091ee5bece13bd24a73f2644e37e48
Tzu-Hung Liang <t110318094@ntut.org.tw>
2023-04-23 16:53:36 +0800

    Rename the files

--- DLE 08 - Register, SRAM/SRAM4Bits.vhdl
+++ DLE 08 - Register, SRAM/SRAM4Bits.vhdl
@@ -2,16 +2,16 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
-entity my_SRAM is
+entity SRAM4bits is
     port(
         D: in std_logic_vector(3 downto 0);
         Address: in std_logic_vector(1 downto 0);
         ChipSelect, WE, OE: in std_logic;
         DO: out std_logic_vector(3 downto 0)
     );
-end my_SRAM;
+end SRAM4bits;
 
-architecture SRAM_four of my_SRAM is
+architecture Behavior of SRAM4bits is
     type memory is array(3 downto 0) of std_logic_vector(3 downto 0);
     signal SRAM_internal: memory := ("0000", "0000", "0000", "0000");
     signal Address_int: integer;
@@ -21,11 +21,13 @@ architecture SRAM_four of my_SRAM is
         Address_temp <= "00" & Address;
         Address_int <= to_integer(unsigned(Address_temp));
 
+        -- Read memory
         DO <= "ZZZZ" when ChipSelect = '0'
         else "ZZZZ" when OE = '0'
         else SRAM_internal(Address_int) when OE = '1';
 
+        -- Write memory
         SRAM_internal(Address_int) <= D when WE = '1';
 
-    end SRAM_four;
+    end Behavior;
     
